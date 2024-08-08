c1e6c7d7494fc37f2ea89635d058692eec6d741f
WhiteCapella <whitecapella.official@gmail.com>
2024-08-08 12:01:01 +0900

    file init

---
+++ dags/pyspark_movie.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+from airflow.operators.python import (
+        PythonOperator,
+        PythonVirtualenvOperator,
+        BranchPythonOperator,
+)
+
+import pandas as pd
+
+with DAG(
+    'pyspark_movie',
+    default_args={
+        'depends_on_past': False,
+        'retries': 1,
+        'retry_delay': timedelta(seconds=3),
+    },
+
+    max_active_runs=1,
+    max_active_tasks=3,
+    description='movie',
+    schedule="10 2 * * *",
+    start_date=datetime(2024, 8, 1),
+    catchup=True,
+    tags=['movie'],
+) as dag:
