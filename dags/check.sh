d770737fec6a2ff39ead5633eebb9690a3154271
WhiteCapella <whitecapella.official@gmail.com>
2024-08-08 11:22:23 +0900

    init

--- dags/check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -f "$HOME/data/signal/{{ds_nodash}}/_DONE ]; then
-	exit 0
-else
-	exit 1
-fi
-
--- dags/mkdone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-mkdir -p "$HOME/data/import_signal/{{ds_nodash}"
-touch $HOME/data/import_signal/{{ds_nodash}}/_DONE
