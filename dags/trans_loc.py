b12b59eadba057fe91eb70a9ba2ff7f8bcdddaef
WhiteCapella <whitecapella.official@gmail.com>
2024-10-07 09:30:16 +0900

    trans_loc

--- dags/trans_loc.py
+++ dags/trans_loc.py
@@ -12,72 +12,66 @@ from airflow.operators.python import (
 )
 from airflow.providers.mysql.hooks.mysql import MySqlHook
 import pandas as pd
+import mysql.connector
+def geom_trans(latitude: str, longitude: str):
+	# lat : 위도
+	# lon : 경도
+	# 결과값 : add - str
+	from de32_3rd_team5.geoutil import loc_trans
 
-def db_login_func():
-    # TODO
-    # 로그인 시도
-    try:
-        conn = mysql.connector.connect(
-            host="13.209.89.75",
-            port=32678,
-            user="pic",
-            password="1234",
-            database="picturedb"
-        )
-        print("MariaDB 연결 성공")
-        conn.close() # 연결 종료
-    except Exception as e:
-        print(f"MariaDB 연결 실패: {e}")
-        raise AirflowException("MariaDB 연결 실패")
+
+def db_connect():
+	conn = mysql.connector.connect(
+		host='13.209.75.216',
+		port='32678',
+		user="pic",
+		password="1234",
+		database="picturedb",
+	)
+	if conn.get_conn():
+		return conn
+	else:
+		return 'login_fail'
 
 def db_check_func():
     # TODO
     # 업데이트 해야하는 DB 체크
-    conn = mysql.connector.connect(
-        host="13.209.89.75",
-        port=32678,
-        user="pic",
-        password="1234",
-        database="picturedb"
-    )
-    with conn.get_conn() as cur:
-        cur.execute("SELECT COUNT(*) FROM picture WHERE address = ''")  
-        # address 컬럼이 비어있는 행 개수 확인
-        empty_count = cur.fetchone()[0]
-    if empty_count == 0:
-        return 'need_not_update'  
-        # 비어있는 행이 없으면 need_not_update 태스크로 이동
-    else:
-        return 'db_update'
+	conn = db_connect()
+	with conn.get_conn() as cur:
+		cur.execute("SELECT COUNT(*) FROM picture WHERE address = ''")  
+		# address 컬럼이 비어있는 행 개수 확인
+		empty_count = cur.fetchone()[0]
+	if empty_count == 0:
+		return 'need_not_update'  
+		# 비어있는 행이 없으면 need_not_update 태스크로 이동
+	else:
+		return 'db_update'
 
 def db_update_func():
+	from de32_3rd_team5.geoutil import loc_trans
     # TODO
     # 업데이트 진행
-    conn = mysql.connector.connect(
-        host="13.209.89.75",
-        port=32678,
-        user="pic",
-        password="1234",
-        database="picturedb"
-    )
+	conn = db_connect()
+	with conn.get_conn() as cur:
+		#TODO
+		# 1. address column이 비어있는 행의 latitude와 longitude를
+		# location = f"{latitude}, {longitude}"의 꼴로 location에 저장
+		
+		# 2. add = trans_loc(location)으로 add를 변환한 후
+		# 3. address에 add를 삽입하기
+		cur.execute("SELECT latitude, longitude FROM picturedb WHERE address IS NULL")
+		rows = cur.fetchall()
 
-def task_succ_aleam():
-    # TODO
-    # 알람 - 작업 완료
+		for row in rows:
+			latitude, longitude = row
+			location = f"{latitude}, {longitude}"
+			add = loc_trans(location)
+			cur.execute("UPDATE picturedb SET address = %s WHERE latitude = %s AND longitude = %s", (add, latitude, longitude))
 
-def login_fail_aleam():
-    # TODO
-    # 알람 - 로그인 실패
+	conn.close_conn()
 
-def nothing_to_update_aleam():
-    # TODO
-    # 알람 - 작업할 필요 없음
-
-def error_aleam():
-    # TODO
-    # 알람 - 작업 에러
 with DAG(
-    'Transfer Location',
+    'Transfer_Location',
     default_args={
         'depends_on_past': False,
         'retries': 0,
@@ -101,32 +95,37 @@ with DAG(
     #         login_fail   nothing_to_update    error
     # start >> db_login >> db_check >>          update >> task_succ >> end
     start = EmptyOperator(task_id='start')
-    end = EmptyOperator(task_id='end')
+    end = EmptyOperator(task_id='end', trigger_rule='all_done')
     
-    db_login = PythonOperator(
-        task_id='db_login',
-        python_callable=db_login_func,
-    )
     db_check = BranchPythonOperator(
         task_id='db.check',
         python_callable=db_check_func,
     )
-    db_update = PythonOperator(
+    db_update = PythonVirtualenvOperator(
         task_id='db.update',
         python_callable=db_update_func,
+		requirements=['git+https://github.com/pladata-encore/DE32-3rd_team5.git@0.1/fastapi_upload_imagefile'],
     )
+    task_succ = BashOperator(
+		task_id='a.succ',
+		bash_command="""
+        curl -X POST -H 'Authorization: Bearer lFAUGd2l1MgZkHf54FJmZEXgyExhjOiqB2ueZlGQe52' -F 'message=task Airflow tasks complete.' https://notify-api.line.me/api/notify
+        """,
+	)
+    login_fail = BashOperator(
+		task_id='e.login',
+		bash_command="""
+        curl -X POST -H 'Authorization: Bearer lFAUGd2l1MgZkHf54FJmZEXgyExhjOiqB2ueZlGQe52' -F 'message=task Airflow tasks fail. Login Error' https://notify-api.line.me/api/notify
+		""",
+		trigger_rule='one_failed',
+	)
+    need_not_update = BashOperator(
+		task_id='a.noupdate',
+		bash_command="""
+        curl -X POST -H 'Authorization: Bearer lFAUGd2l1MgZkHf54FJmZEXgyExhjOiqB2ueZlGQe52' -F 'message=task Airflow tasks will close. Databases are not need update' https://notify-api.line.me/api/notify
+		""",
+	)
 
-    task_succ = PythonOperator(
-        task_id='task.suc',
-        python_callable=task_succ_func,
-    )
-
-    login_fail = EmptyOperator(task_id='login.fail')
-    need_not_update = EmptyOperator(task_id='nnu')
-    error = EmptyOperator(task_id='error')
-
-
-start >> db_login >> db_check >> db_update >> task_succ >> end
-db_login >> login_fail >> end
+start >> db_check >> db_update >> task_succ >> end
+login_fail >> end
 db_check >> need_not_update >> task_succ >> end
-db_update >> error >> end
